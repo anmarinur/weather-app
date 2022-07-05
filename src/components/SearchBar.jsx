807fbff21bbe8815705e3d4c199f22e0521b6c62
anmarinur <andersonmarindev@gmail.com>
2022-07-05 15:58:55 -0500

    Add style to searchBar

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -11,13 +11,18 @@ function SearchBar(prop) {
   return (
     <div className={style.main}>
       <input className={style.input} type="text" placeholder="Ciudad..." onChange={handleInputChange} value={city}></input>
-      <button className={style.button} onClick={e => 
+        <div className={style.button} onClick={e => 
         {
-          e.preventDefault();
           prop.onSearch(city);
           setCity('');
         }
-      }>Agregar</button>
+        }>
+        <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-search" width="32" height="32" viewBox="0 0 24 24" stroke-width="2" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
+        <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+        <circle cx="10" cy="10" r="7" />
+        <line x1="21" y1="21" x2="15" y2="15" />
+        </svg>
+      </div>
     </div>
   );
 }
