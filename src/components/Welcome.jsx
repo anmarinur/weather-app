bc8b47f091906336c2febb4750bb4b8522082e27
anmarinur <andersonmarindev@gmail.com>
2022-07-08 22:39:42 -0500

    Change the favicon

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -5,7 +5,7 @@ function Welcome() {
   return (
     <div className={style.div}>
       <h1 className={style.h1}>Bienvenido a <span className={style.span}>The Weather App</span></h1>
-      <p className={style.p}>Para empezar escribe en la barra de búsqueda la ciudad que quieres consultar y obtendrás una tarjeta con sus datos climáticos. Puedes agregar tantas ciudades como quieras. Si quieres tener más información de la ciudad que buscaste, da clic encima y te mostrará más datos climáticos. Tambien puedes iniciar sesión para guardar tus preferencias (esta parte en desarrollo).</p>
+      <p className={style.p}>Para empezar, escribe en la barra de búsqueda la ciudad que quieres consultar y obtendrás una tarjeta con sus datos climáticos. Puedes agregar tantas ciudades como quieras. Si quieres tener más información de la ciudad que buscaste, da clic encima y te mostrará más datos climáticos. Tambien puedes iniciar sesión para guardar tus preferencias (esta parte en desarrollo).</p>
     </div>
   );
 }
